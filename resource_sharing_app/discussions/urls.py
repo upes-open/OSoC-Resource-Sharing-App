68f1bbd1061e27c8e822ed4f88b956b40f394a1a
AnanyaM12 <ananyaamathur03@gmail.com>
2023-08-03 01:48:42 +0530

    issue #33 solved

--- resource_sharing_app/discussions/urls.py
+++ resource_sharing_app/discussions/urls.py
@@ -3,8 +3,10 @@ from discussions import views
 
 urlpatterns = [
    path('comment/create/<int:pk>/', views.create_comment, name='create_comment'),
+    path('comment/delete/<int:pk>/', views.delete_comment, name='delete_comment'),
    #path('resource/', views.resource_detail, name='resource_detail'),
    path('create/discussion', views.create_discussion, name='create_discussion'),
+   path('delete/discussion/<int:pk>/', views.delete_discussion, name='delete_discussion'),
    path('display/discussion/<int:pk>/', views.discussion_detail, name='discussion_detail'),
    path('list/', views.discussion_list, name='discussion_list'),
 
--- resource_sharing_app/discussions/views.py
+++ resource_sharing_app/discussions/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Comment, Discussion, Resource
 from .forms import CommentForm, CreateDiscussionForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
 
 def create_comment(request, pk):
     discussion = get_object_or_404(Discussion, pk=pk)
@@ -21,34 +23,75 @@ def create_comment(request, pk):
     else:
         form = CommentForm()
 
-    return render(request, 'discussion_detail.html', {'discussion': discussion, 'comments': comments, 'form': form})
+    return render(request, 'discussions/discussion_detail.html', {'discussion': discussion, 'comments': comments, 'form': form})
+
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+
+    if request.method == 'POST':
+        # Only allow the author of the comment or a superuser to delete it
+        if comment.author == request.user or request.user.is_superuser:
+            comment.delete()
+            messages.success(request, 'Comment deleted successfully.')
+            return redirect('discussion_detail', pk=comment.discussion.pk)
+        else:
+            try:
+                # Attempt to delete the comment to trigger the PermissionDenied exception
+                comment.delete()
+            except PermissionDenied:
+                messages.error(request, 'You do not have permission to delete this comment.')
+
+    return render(request, 'discussions/delete_comment.html', {'comment': comment})
 
 def resource_detail(request):
     resource = get_object_or_404(Resource)
     discussions = Discussion.objects.filter(resource=resource)
-    return render(request, 'resource_detail.html', {'resource': resource, 'discussions': discussions})
+    return render(request, 'discussions/resource_detail.html', {'resource': resource, 'discussions': discussions})
 
 def create_discussion(request):
     if request.method == 'POST':
         form = CreateDiscussionForm(request.POST)
         if form.is_valid():
-            # Save the new discussion to the database
             discussion_id = form.cleaned_data['discussion_id']
-            title = form.cleaned_data['title']
-            discussion = Discussion.objects.create(discussion_id=discussion_id, title=title)
 
-            # Redirect to the discussion detail page after creating the discussion
-            return redirect('discussion_detail', pk=discussion.pk)
+            # Check if the discussion ID already exists in the database
+            if Discussion.objects.filter(discussion_id=discussion_id).exists():
+                messages.error(request, f"A discussion with ID '{discussion_id}' already exists.")
+            else:
+                title = form.cleaned_data['title']
+                discussion = Discussion.objects.create(discussion_id=discussion_id, title=title)
+
+                # Redirect to the discussion list page after creating the discussion
+                return redirect('discussion_list')
+        else:
+            # If the form is not valid, redisplay the form with errors
+            form = CreateDiscussionForm(request.POST)
     else:
         form = CreateDiscussionForm()
 
-    return render(request, 'create_discussion.html', {'form': form})
+    return render(request, 'discussions/create_discussion.html', {'form': form,'messages': messages.get_messages(request)})
+
+def delete_discussion(request, pk):
+    discussion = get_object_or_404(Discussion, pk=pk)
+
+    if request.method == 'POST':
+        # Only allow the author of the discussion to delete it
+        if discussion.author == request.user:
+            discussion.delete()
+            messages.success(request, 'Discussion deleted successfully.')
+            return redirect('discussion_list')
+        else:
+            messages.error(request, 'You do not have permission to delete this discussion.')
+
+    return render(request, 'discussions/delete_discussion.html', {'discussion': discussion})
+
 
 def discussion_detail(request, pk):
     discussion = get_object_or_404(Discussion, pk=pk)
     comments = Comment.objects.filter(discussion=discussion, parent=None)
-    return render(request, 'discussion_detail.html', {'discussion': discussion, 'comments': comments})
+    return render(request, 'discussions/discussion_detail.html', {'discussion': discussion, 'comments': comments})
 
 def discussion_list(request):
     discussions = Discussion.objects.all()
-    return render(request, 'discussion_list.html', {'discussions': discussions})
+    return render(request, 'discussions/discussion_list.html', {'discussions': discussions})
